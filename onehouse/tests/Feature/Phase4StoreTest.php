8398a3181cc1553376b350ada9a76944874011e6
yuzu <yuzuha.1415s@gmail.com>
2025-11-25 18:38:11 +0900

    phase4削除のテスト完了

--- onehouse/tests/Feature/Phase4StoreTest.php
+++ onehouse/tests/Feature/Phase4StoreTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\LandLog;
 use App\Models\Profile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -20,7 +21,7 @@ class Phase4StoreTest extends TestCase
         $response->assertStatus(200);
     }
 
-    // HTTP リクエストを作る（POST /phase4）
+    // HTTPリクエストを作る（POST /phase4）
     public function test_post_request()
     {
         $profile = Profile::factory()->create([
@@ -39,6 +40,8 @@ class Phase4StoreTest extends TestCase
             'builable_area' => '192',
             'pricePerTsubo' => '80',
         ]);
+
+        // DB にレコードがあるか確認する
         $response->assertStatus(302);
         $this->assertDatabaseHas('landlogs', [
             'profile_id' => $profile->id,
@@ -51,12 +54,41 @@ class Phase4StoreTest extends TestCase
             'pricePerTsubo' => '80',
 
         ]);
+        // session に成功メッセージがあるか確認する
+        $response->assertSessionHas('success', '登録が完了しました');
     }
-    // DB にレコードがあるか確認する
+    //  削除するDELETE
+    public function test_delete_request()
+    {
+        $profile = Profile::factory()->create([
+            'id' => 1,
+            'user_id' => 1,
+        ]);
 
-    // ession に成功メッセージがあるか確認する
+        $response = $this->post('/phase4', [
+            'profile_id' => $profile->id,
+            'address' => '123-4567 東京都渋谷区56-4523 5丁目',
+            'landarea' => '120',
+            'far' => '200',
+            'bcr' => '80',
+            'floor' => '2',
+            'builable_area' => '192',
+            'pricePerTsubo' => '80',
+        ]);
+        $landlog = LandLog::latest()->first();
 
 
-    // 正しいページにリダイレクトされたか確認する
+        $response = $this->delete("/phase4/{$landlog->id}");
+        $response->assertStatus(302);
+
+        $this->assertDatabaseMissing('landlogs', [
+            'id' => $landlog->id,
+        ]);
+
+
+        $response->assertSessionHas('success', "{$landlog->address}を削除しました");
+    }
+
+    // 更新するPUT
 
 }
