4a3651dde10769eff94571d3172d8f1409e9ecbb
yuzu <yuzuha.1415s@gmail.com>
2025-11-26 19:49:16 +0900

    JSの計算ロジックをテスト

--- onehouse/resources/js/tests/calculate.test.js
+++ onehouse/resources/js/tests/calculate.test.js
@@ -12,15 +12,41 @@ describe("", () => {
 
         expect(result.maxFloorArea).toBe(60);
         expect(result.buildableArea).toBe(120);
-        // buildableAreaTsubo
-
-        // buildingCost =
+        expect(result.buildableAreaTsubo).toBe(36);
+        expect(result.buildingCost).toBe(3163);
     });
-    it("// 入力が０または負の場合の扱い", ()=> {
 
+    it("入力が０または負の場合の扱い", () => {
+        const result = calculate({
+            landArea: -100,
+            far: 100,
+            bcr: 50,
+            pricePerTsubo: 100,
+        });
+        expect(result.error).toBe(true);
+    });
+
+    it("小数点処理が正しいか", () => {
+        const result = calculate({
+            landArea: 100.0,
+            far: 100.0,
+            bcr: 50.0,
+            pricePerTsubo: 100.0,
+        });
+        expect(result.maxFloorArea).toBe(Math.floor( 100.0 * (50.0 / 100)));
+        expect(result.buildableArea).toBe(Math.floor(50.0 * (100.0 / 100)));
+        expect(result.buildableAreaTsubo).toBe(Math.floor( 50.0 / 3.3));
+        expect(result.buildingCost).toBe(Math.floor((50.0 / 3.3) * 100.0));
+
+
+    });
+    it("予想外の値(異常系)", () => {
+        const result = calculate({
+            landArea: "ab",
+            far: NaN,
+            bcr: 50,
+            pricePerTsubo: 100,
+        });
+        expect(result.error).toBe(true);
     });
 });
-
-
-// 小数点処理が正しいか
-// 予想外の値(異常系)
