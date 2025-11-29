dd4eade4db6dc1ed183926e5fead42be2d17d432
yuzu <yuzuha.1415s@gmail.com>
2025-11-29 20:33:08 +0900

    vite.comfig.jsにテストファイルを追記

--- onehouse/vite.config.js
+++ onehouse/vite.config.js
@@ -5,7 +5,11 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/landSimulation.js",
+            ],
             refresh: true,
         }),
         vue(),
