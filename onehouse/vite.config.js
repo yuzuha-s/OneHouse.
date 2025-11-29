ba8860a44b70c076d649ff15ff217c2654ec281b
yuzu <yuzuha.1415s@gmail.com>
2025-11-29 20:37:19 +0900

    vite.comfig.jsにstyle.cssを追記

--- onehouse/vite.config.js
+++ onehouse/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/css/app.css",
+                "resources/css/style.css",
                 "resources/js/app.js",
                 "resources/js/landSimulation.js",
             ],
