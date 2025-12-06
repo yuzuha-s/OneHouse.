10618a4ea67512f3bf8876e4393f5f59fb03c0c5
yuzu <yuzuha.1415s@gmail.com>
2025-12-06 13:30:44 +0900

    デプロイ修正

--- onehouse/app/Providers/AppServiceProvider.php
+++ onehouse/app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Checklist;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+// use Illuminate\Support\Facades\URL; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
     // チェックリストをすべてのビューで表示する
     public function boot(): void
     {
+        // if (config('app.force_https', false)) {
+        //     URL::forceScheme('https');
+        // }
         if (Schema::hasTable('checklists')) {
             $checkLists = Checklist::all();
             View::share('checkLists', $checkLists);
